2d8b8dae03b73d38f114168b86810f243019d279
filippabostrom <bostromfilippa@gmail.com>
2023-10-18 10:41:49 +0200

    Libary form och kategori form

--- PodProject/AddPodPage.Designer.cs
+++ PodProject/AddPodPage.Designer.cs
@@ -43,10 +43,10 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Stencil", 16.125F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(380, 39);
+            label1.Font = new Font("Times New Roman", 16.125F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(427, 37);
             label1.Name = "label1";
-            label1.Size = new Size(361, 51);
+            label1.Size = new Size(282, 49);
             label1.TabIndex = 0;
             label1.Text = "Lägg till podd";
             // 
@@ -83,47 +83,47 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Stencil", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
             label2.Location = new Point(295, 165);
             label2.Name = "label2";
-            label2.Size = new Size(88, 38);
+            label2.Size = new Size(93, 36);
             label2.TabIndex = 5;
             label2.Text = "URL:";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Stencil", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(269, 269);
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(278, 268);
             label3.Name = "label3";
-            label3.Size = new Size(114, 38);
+            label3.Size = new Size(110, 36);
             label3.TabIndex = 6;
             label3.Text = "Namn:";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Stencil", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(200, 377);
+            label4.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(240, 375);
             label4.Name = "label4";
-            label4.Size = new Size(183, 38);
+            label4.Size = new Size(148, 36);
             label4.TabIndex = 7;
             label4.Text = "Kategori:";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Stencil", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(189, 489);
+            label5.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(244, 489);
             label5.Name = "label5";
-            label5.Size = new Size(194, 38);
+            label5.Size = new Size(144, 36);
             label5.TabIndex = 8;
             label5.Text = "Intervall:";
             // 
             // btnAdd
             // 
             btnAdd.BackColor = Color.Snow;
-            btnAdd.Font = new Font("Stencil", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnAdd.Font = new Font("Times New Roman", 9F, FontStyle.Bold, GraphicsUnit.Point);
             btnAdd.Location = new Point(458, 580);
             btnAdd.Name = "btnAdd";
             btnAdd.Size = new Size(230, 46);
@@ -136,7 +136,7 @@
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LightSteelBlue;
-            ClientSize = new Size(1139, 802);
+            ClientSize = new Size(1084, 779);
             Controls.Add(btnAdd);
             Controls.Add(label5);
             Controls.Add(label4);
@@ -149,6 +149,7 @@
             Controls.Add(label1);
             Name = "AddPodPage";
             Text = "AddPodPage";
+            Load += AddPodPage_Load;
             ResumeLayout(false);
             PerformLayout();
         }
